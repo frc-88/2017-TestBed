1576bb18b151ba9bc01d806b3f557172d6f709ff
MrNick <navtges@gmail.com>
2017-01-12 22:49:17 -0500

    Shooter framework

---
+++ src/org/usfirst/frc/team88/robot/commands/Stop.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team88.robot.commands;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+
+/**
+ *
+ */
+public class Stop extends InstantCommand {
+
+    public Stop() {
+        super();
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called once when the command executes
+    protected void initialize() {
+    }
+
+}
