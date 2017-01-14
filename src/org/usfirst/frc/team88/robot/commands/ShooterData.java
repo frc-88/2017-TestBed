bc417807acd85a6391bbe5c7af54368ef4f58838
MrNick <navtges@gmail.com>
2017-01-14 09:43:48 -0500

    Added ShooterData Method

---
+++ src/org/usfirst/frc/team88/robot/commands/ShooterData.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team88.robot.commands;
+
+import org.usfirst.frc.team88.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class ShooterData extends Command {
+
+    public ShooterData() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.shooter);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+		SmartDashboard.putDouble("Left Input", Robot.oi.getDriverLeftVerticalAxis());
+		SmartDashboard.putDouble("Right Input", Robot.oi.getDriverRightVerticalAxis());
+		SmartDashboard.putDouble("Left Maxed", Robot.oi.applyMaxValue(Robot.oi.getDriverLeftVerticalAxis()));
+		SmartDashboard.putDouble("Right Maxed", Robot.oi.applyMaxValue(Robot.oi.getDriverRightVerticalAxis()));
+		
+		Robot.shooter.displayData();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
